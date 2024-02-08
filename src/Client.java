63fa81e27ae3893e1b01c1be5f50ee9952473ecc
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-08 08:43:49 +0000

    Creating the sendMessage class

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,8 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -10,16 +13,42 @@ public class Client {
     private String username;
 
     // constructor
-    public Client(Socket socket, String username){
-        try{
+    public Client(Socket socket, String username) {
+        try {
             this.socket = socket;
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.username = username;
-        } catch(IOException e){
+        } catch (IOException e) {
+            e.printStackTrace();
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    // Method to send message
+    public void sendMessage(){
+        try{
+            // send the username first
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            // getting the input from the console
+            Scanner scanner = new Scanner(System.in);
+
+            while(socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.close();
+            }
+
+
+        }catch(IOException e){
             e.printStackTrace();
             closeEverything(socket, bufferedReader, bufferedWriter);
 
+        }
     }
 
 }
