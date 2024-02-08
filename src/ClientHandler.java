7535b39bf1529a25b41670dbb9bde34a769bd15b
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-08 08:23:12 +0000

    Creating the constructor in the ClientHandler class

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,30 @@
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+
+    // Keeps track of all the clients
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+
+    // Constructor
+    public ClientHandler(Socket socket){
+        try{
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
