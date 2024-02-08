288af6a0f70caae6b29136d1ab9a326924022d1e
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-08 08:47:41 +0000

    Creating the closeEverything method

--- src/Client.java
+++ src/Client.java
@@ -73,4 +73,22 @@ public class Client {
         }).start();
     }
 
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+
+            if(bufferedWriter != null){
+                bufferedWriter.close();
+            }
+
+            if(socket != null){
+                socket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
